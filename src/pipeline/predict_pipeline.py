db55be0306d3a142833518c5c9c71e3b05f4724b
unknown <pallavimohantypallu31@gmail.com>
2023-11-15 17:42:14 +0530

    Deployment configuration for AWS

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
@@ -10,14 +11,26 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path="artifacts","model.pkl"
-            preprocessor_path='artifacts\preprocessor.pkl'
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts/proprocessor.pkl')
+            print("Before Loading")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
             return preds
-        
+
+#    def predict(self,features):
+#        try:
+#            model_path="artifacts","model.pkl"
+#            preprocessor_path='artifacts\preprocessor.pkl'
+#            model=load_object(file_path=model_path)
+#            preprocessor=load_object(file_path=preprocessor_path)
+#            data_scaled=preprocessor.transform(features)
+#            preds=model.predict(data_scaled)
+#            return preds
+#        
         except Exception as e:
             raise CustomException(e,sys)
 
